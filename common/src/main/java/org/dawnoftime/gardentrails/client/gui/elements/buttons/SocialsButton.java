ea984c4646ad5bbebcb46fa50949ead9aeb864d2
pchag <p.chagnon@orange.fr>
2025-10-08 01:04:12 +0200

    Fixed socials tooltip

--- common/src/main/java/org/dawnoftime/gardentrails/client/gui/elements/buttons/SocialsButton.java
+++ common/src/main/java/org/dawnoftime/gardentrails/client/gui/elements/buttons/SocialsButton.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
+import net.minecraft.client.gui.screens.inventory.tooltip.DefaultTooltipPositioner;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
@@ -41,5 +43,10 @@ public class SocialsButton extends Button {
 
             pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
         }
+    }
+
+    @Override
+    protected @NotNull ClientTooltipPositioner createTooltipPositioner() {
+        return DefaultTooltipPositioner.INSTANCE;
     }
 }
